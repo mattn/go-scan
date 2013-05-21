3af9b9b181eb38a9d2d641eb7269fe07055f4dcd
mattn <mattn.jp@gmail.com>
2013-05-21 21:18:00 +0900

    Add test code

---
+++ scan_test.go
@@ -0,0 +1,120 @@
+package scan_test
+
+import (
+	"."
+	"encoding/json"
+	"reflect"
+	"testing"
+)
+
+var ScanTests = []struct {
+	j string
+	p string
+	v interface{}
+} {
+	{
+		"foo",
+		"bar",
+		"baz",
+	},
+}
+
+func TestScanString(t *testing.T) {
+	var a interface{}
+	j :=
+`
+"foo"
+`
+	err := json.Unmarshal([]byte(j), &a)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var s string
+	err = scan.Scan(a, &s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if s != "foo" {
+		t.Fatalf("Expected %v for Scan, but %v:", "foo", s)
+	}
+}
+
+func TestScanFloat64(t *testing.T) {
+	var a interface{}
+	j :=
+`
+123
+`
+	err := json.Unmarshal([]byte(j), &a)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var f float64
+	err = scan.Scan(a, &f)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if f != 123.0 {
+		t.Fatalf("Expected %f for Scan, but %f:", 123.0, f)
+	}
+}
+
+func TestScanTreeMap(t *testing.T) {
+	var a interface{}
+	j :=
+`
+{"foo":{"bar": "baz"}}
+`
+	err := json.Unmarshal([]byte(j), &a)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var s string
+	err = scan.ScanTree(a, "/foo/bar", &s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if s != "baz" {
+		t.Fatalf("Expected %v for Scan, but %v:", "baz", s)
+	}
+}
+
+func TestScanTreeSliceOfString(t *testing.T) {
+	var a interface{}
+	j :=
+`
+{"foo":{"bar": ["baz", "baba"]}}
+`
+	err := json.Unmarshal([]byte(j), &a)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var s []string
+	err = scan.ScanTree(a, "/foo/bar", &s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual([]string{"baz", "baba"}, s) {
+		t.Fatalf("Expected %v for Scan, but %v:", `["bar": "baba"]`, s)
+	}
+}
+
+func TestScanTreeSliceOfFloat64(t *testing.T) {
+	var a interface{}
+	j :=
+`
+{"foo":{"bar": [3, 2, 1]}}
+`
+	err := json.Unmarshal([]byte(j), &a)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var f []float64
+	err = scan.ScanTree(a, "/foo/bar", &f)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual([]float64{3, 2, 1}, f) {
+		t.Fatalf("Expected %v for Scan, but %v:", `[3, 2, 1]`, f)
+	}
+}
